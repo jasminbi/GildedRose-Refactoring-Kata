20020283ad08f6440b2f16af0f45fa30c29c6cfb
jasminbi <jasmin.bissig@gmail.com>
2018-04-19 11:01:12 +0200

    Add tests for Backstage passes

--- Java/src/test/java/com/gildedrose/GildedRoseShould.java
+++ Java/src/test/java/com/gildedrose/GildedRoseShould.java
@@ -91,6 +91,46 @@ public class GildedRoseShould {
 
     }
 
+    @Test
+    public void increase_quality_if_item_is_backstage_passes() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    public void increase_quality_by_2_if_item_is_backstage_passes_10_days_before_sellin() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(7, app.items[0].quality);
+    }
+
+    @Test
+    public void increase_quality_by_3_if_item_is_backstage_passes_5_days_before_sellin() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    public void drop_quality_to_0_if_item_is_backstage_passes_after_sellin() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
 
 
 
