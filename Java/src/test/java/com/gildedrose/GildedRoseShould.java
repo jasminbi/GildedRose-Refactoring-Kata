e172b8ee58932f0e4e0347eb51e0e7c5559f63cd
jasminbi <jasmin.bissig@gmail.com>
2018-04-19 10:49:10 +0200

    Add tests for faster decrease, limits and Aged Brie

--- Java/src/test/java/com/gildedrose/GildedRoseShould.java
+++ Java/src/test/java/com/gildedrose/GildedRoseShould.java
@@ -19,6 +19,58 @@ public class GildedRoseShould {
         assertEquals(4, app.items[0].quality);
     }
 
+    @Test
+    public void decrease_sellin_daily() {
+        Item[] items = new Item[] { new Item("foo", 10, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+    @Test
+    public void decrease_quality_faster_after_sellin() {
+        Item[] items = new Item[] { new Item("foo", 0, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(3, app.items[0].quality);
+    }
+
+    @Test
+    public void limit_quality_decrease_to_0() {
+        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void increase_quality_if_item_is_brie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 5) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    public void limit_item_quality_to_50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+
+
 
 
 
